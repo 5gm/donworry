80d715da66e9b8b1c92bd342667654ad4dcd862f
5gm <79223619+5gm@users.noreply.github.com>
2025-04-21 00:35:48 +1200

    Update page.tsx

--- app/card2/page.tsx
+++ app/card2/page.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import React from "react";
 import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
-import ViewCount from '../components/viewCount';
+import ViewCount from '../app/viewCount';
 
 function FramedCard () {
   return (
