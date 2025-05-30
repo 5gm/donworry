03303ea9333ec9c4a238a915f86ecb4da62f0881
5gm <79223619+5gm@users.noreply.github.com>
2025-05-30 17:59:23 +1200

    Update loader.lua

--- public/loader.lua
+++ public/loader.lua
@@ -3,7 +3,7 @@ local Supported = {
     [7580655576] = "https://raw.githubusercontent.com/5gm/scripts/main/Pet%20Crystal%20simulatorui2.lua",
     [7463504342] = "https://raw.githubusercontent.com/5gm/scripts/refs/heads/main/Pet%20Incrementalui2.lua",
     [7114126212] = "https://raw.githubusercontent.com/5gm/scripts/refs/heads/main/Freekick%20simulator.lua",
-    [7541395924] = "https://raw.githubusercontent.com/5gm/scripts/refs/heads/main/Build%20An%20Island",
+    [7541395924] = "https://raw.githubusercontent.com/5gm/scripts/refs/heads/main/Build%20An%20Island.lua",
 
     -- Add more games below following the same format:
     -- [1234567890] = "https://raw.githubusercontent.com/username/repo/main/script.lua",
