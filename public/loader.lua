fddc39ee0d5d02e7cdedd14ae44eba8280238f19
5gm <79223619+5gm@users.noreply.github.com>
2025-06-10 17:37:46 +1200

    Update loader.lua

--- public/loader.lua
+++ public/loader.lua
@@ -5,6 +5,8 @@ local Supported = {
     [7114126212] = "https://raw.githubusercontent.com/5gm/scripts/refs/heads/main/Freekick%20simulator.lua",
     [7541395924] = "https://raw.githubusercontent.com/5gm/scripts/refs/heads/main/Build%20An%20Island.lua",
     [7679508751] = "https://raw.githubusercontent.com/5gm/scripts/refs/heads/main/Infinite%20Mining%20Incremental.lua",
+    [7799183193] = "https://raw.githubusercontent.com/5gm/scripts/refs/heads/main/Steal%20a%20car.lua",
+    
 
     -- Add more games below following the same format:
     -- [1234567890] = "https://raw.githubusercontent.com/username/repo/main/script.lua",
